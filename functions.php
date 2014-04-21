9c30d116fa2ecd536148924fe158b7314f5c563c
Pete Lower <pete.lower@gmail.com>
2014-04-21 15:29:28 -0500

    Add read more functionality

--- functions.php
+++ functions.php
@@ -2,4 +2,9 @@
 
 // Add excerpts to Meteor Slides
 add_post_type_support('slide', 'excerpt');
-//----------------------------//
+
+//Adjust readmore text
+function new_excerpt_more( $more ) {
+	return ' <a class="read-more" href="'. get_permalink( get_the_ID() ) . '">' . __('Read More', 'your-text-domain') . '</a>';
+}
+add_filter( 'excerpt_more', 'new_excerpt_more' );
