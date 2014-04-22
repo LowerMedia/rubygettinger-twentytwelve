2c232573cf15cf86b51dc6428c98168ea0ec2733
root <root@s15809313.onlinehome-server.com>
2014-04-22 10:25:38 -0500

    adjusting the content template

--- content.php
+++ content.php
@@ -17,9 +17,11 @@
 		</div>
 		<?php endif; ?>
 		<header class="entry-header">
-			<?php if ( ! post_password_required() && ! is_attachment() ) :
-				the_post_thumbnail();
-			endif; ?>
+			<?php 
+				if ( ! post_password_required() && ! is_attachment() ) :
+					if ( is_category() || is_archive() ) {the_post_thumbnail('thumbnail');}
+				endif; 
+			?>
 
 			<?php if ( is_single() ) : ?>
 			<h1 class="entry-title"><?php the_title(); ?></h1>
